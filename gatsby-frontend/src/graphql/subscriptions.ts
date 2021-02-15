62672abff3a9b349977733f9cc93df9b2575866f
AhsenRiaz <ahsenriaz2001@gmail.com>
2021-02-16 04:19:56 +0500

    updated

---
+++ gatsby-frontend/src/graphql/subscriptions.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onAddProduct = /* GraphQL */ `
+  subscription OnAddProduct {
+    onAddProduct {
+      id
+      title
+      done
+    }
+  }
+`;
